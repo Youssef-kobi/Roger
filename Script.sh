669aab5fe2d1cd10341793d88c9ef0d39ed933c2
youssef <Youssefkobi@gmail.com>
2019-10-20 23:52:19 +0000

    f

--- Script.sh
+++ Script.sh
@@ -1,5 +1,11 @@
+
 #add user to sudoers
-echo Add a user name(this will be added to sudoers group):
+echo Add a user name\(this will be added to sudoers group\):
 read varname
 adduser $varname
 usermod -aG sudo $varname
+echo $varname is a sudoer !
+echo Getting Your Static ip ready ...
+cp ./50-cloud-init.yaml /etc/netplan/50-cloud-init.yaml
+netplan apply
+echo netplan applied
