50799e728ba858ff839e9b2c09c8221c38e860e0
Youssef El kobi <yel-kobi@e1r8p10.1337.ma>
2019-10-22 12:53:35 +0100

    ff

--- Script.sh
+++ Script.sh
@@ -4,25 +4,19 @@
 #useradd $varname
 #usermod -aG sudo $varname
 #echo $varname is a sudoer !
-##Static ip
-#echo Getting Your Static ip ready ...
-#cp ~/Roger/50-cloud-init.yaml /etc/netplan/50-cloud-init.yaml
-#netplan apply
-#echo netplan applied
-##SSH
-#echo Changing SSH port to 2211 and editing the file sshd-config to block RootLoginPermit, PasswordAuthentification and uncomment PublicKeyAuthuntification
-#cp ~/Roger/sshd_config /etc/ssh/sshd_config
-#echo applying public key \(id_rsa.pub\)
-#cp ~/Roger/id_rsa.pub  ~/.ssh/authorized_keys
-##setting up iptables rules
+#Static ip
+echo Getting Your Static ip ready ...
+cp ./50-cloud-init.yaml /etc/netplan/50-cloud-init.yaml
+netplan apply
+echo netplan applied
+#SSH
+echo Changing SSH port to 2211 and editing the file sshd-config to block RootLoginPermit, PasswordAuthentification and uncomment PublicKeyAuthuntification
+cp ./sshd_config /etc/ssh/sshd_config
+echo applying public key \(id_rsa.pub\)
+cp ./id_rsa.pub  ~/.ssh/authorized_keys
+#setting up iptables rules
 echo setting up iptables firewall Dos protection and for portscan 
 iptables -F
-iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 53 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 2211 -j ACCEPT
-iptables -A INPUT -p udp -m udp --dport 53 -j ACCEPT
 iptables -A INPUT -m state --state NEW -m recent --set
 iptables -A INPUT -p tcp -m state --state NEW -m recent --update --seconds 1 --hitcount 10 -j DROP
 iptables -A INPUT -p udp -m udp --dport 53 -m state --state NEW -m recent --set 
@@ -34,9 +28,14 @@ iptables -A INPUT -p tcp -m tcp --dport 443 -m state --state NEW -m recent --upd
 iptables -A INPUT -p tcp -m tcp --dport 80 -m state --state NEW -m recent --set
 iptables -A INPUT -p tcp -m tcp --dport 80 -m state --state NEW -m recent --update --seconds 60 --hitcount 10 -j DROP
 iptables -A INPUT -p tcp -m tcp --dport 2211 -m state --state NEW -m recent --update --seconds 60 --hitcount 10 -j DROP
-i
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT
+iptables -A INPUT -p tcp -m tcp --dport 53 -j ACCEPT
+iptables -A INPUT -p tcp -m tcp --dport 2211 -j ACCEPT
+iptables -A INPUT -p udp -m udp --dport 53 -j ACCEPT
 echo saving iptables rulees with iptables-persistent
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
